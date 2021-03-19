f965f3acfc681867a48e80a6ac4d99237e015562
HelanoCarvalho <helanonucleo@gmail.com>
2021-03-19 15:42:26 -0300

    Quase

---
+++ home/home.js
@@ -0,0 +1,168 @@
+function mensagem(){
+    removeEventListener('mouseenter', mensagem)
+    var fig = document.getElementById('fig')
+    var msg = document.createElement('div')
+    msg.style.opacity = 0
+    msg.setAttribute("id", "msg")
+    var p = document.createElement('p')
+    var txt = document.createTextNode("Frete grátis e encomenda sob medida")
+    setTimeout(function(){
+        p.appendChild(txt)
+        msg.appendChild(p)
+        fig.appendChild(msg)
+    }, 1000);
+
+   setTimeout(function(){
+    msg.style.opacity = 1
+   }, 2000);
+
+     setTimeout(function() {
+        msg.style.opacity = 0
+    }, 7000);
+
+    setTimeout(function() {
+        msg.remove()
+    }, 9000);
+}
+
+
+
+function mensagem2(){
+    removeEventListener('mouseenter', mensagem2)
+    var fig = document.getElementById('fig2')
+    var msg = document.createElement('div')
+    msg.style.opacity = 0
+    msg.setAttribute("id", "msg")
+    var p = document.createElement('p')
+    var txt = document.createTextNode("Frete grátis e encomenda sob medida")
+    setTimeout(function(){
+        p.appendChild(txt)
+        msg.appendChild(p)
+        fig.appendChild(msg)
+    }, 1000);
+
+   setTimeout(function(){
+    msg.style.opacity = 1
+   }, 2000);
+
+     setTimeout(function() {
+        msg.style.opacity = 0
+    }, 7000);
+
+    setTimeout(function() {
+        msg.remove()
+    }, 9000);
+    
+}
+
+function mensagem3(){
+    removeEventListener('mouseenter', mensagem3)
+    var fig = document.getElementById('fig3')
+    var msg = document.createElement('div')
+    msg.style.opacity = 0
+    msg.setAttribute("id", "msg")
+    var p = document.createElement('p')
+    var txt = document.createTextNode("Frete grátis e encomenda sob medida")
+    setTimeout(function(){
+        p.appendChild(txt)
+        msg.appendChild(p)
+        fig.appendChild(msg)
+    }, 1000);
+
+   setTimeout(function(){
+    msg.style.opacity = 1
+   }, 2000);
+
+     setTimeout(function() {
+        msg.style.opacity = 0
+    }, 7000);
+
+    setTimeout(function() {
+        msg.remove()
+    }, 9000);
+    
+}
+
+function mensagem4(){
+    removeEventListener('mouseenter', mensagem4)
+    var fig = document.getElementById('fig4')
+    var msg = document.createElement('div')
+    msg.style.opacity = 0
+    msg.setAttribute("id", "msg")
+    var p = document.createElement('p')
+    var txt = document.createTextNode("Frete grátis e encomenda sob medida")
+    setTimeout(function(){
+        p.appendChild(txt)
+        msg.appendChild(p)
+        fig.appendChild(msg)
+    }, 1000);
+
+   setTimeout(function(){
+    msg.style.opacity = 1
+   }, 2000);
+
+     setTimeout(function() {
+        msg.style.opacity = 0
+    }, 7000);
+
+    setTimeout(function() {
+        msg.remove()
+    }, 9000);
+    
+}
+
+function mensagem5(){
+    removeEventListener('mouseenter', mensagem5)
+    var fig = document.getElementById('fig5')
+    var msg = document.createElement('div')
+    msg.style.opacity = 0
+    msg.setAttribute("id", "msg")
+    var p = document.createElement('p')
+    var txt = document.createTextNode("Frete grátis e encomenda sob medida")
+    setTimeout(function(){
+        p.appendChild(txt)
+        msg.appendChild(p)
+        fig.appendChild(msg)
+    }, 1000);
+
+   setTimeout(function(){
+    msg.style.opacity = 1
+   }, 2000);
+
+     setTimeout(function() {
+        msg.style.opacity = 0
+    }, 7000);
+
+    setTimeout(function() {
+        msg.remove()
+    }, 9000);
+    
+}
+
+function mensagem6(){
+    removeEventListener('mouseenter', mensagem6)
+    var fig = document.getElementById('fig6')
+    var msg = document.createElement('div')
+    msg.style.opacity = 0
+    msg.setAttribute("id", "msg")
+    var p = document.createElement('p')
+    var txt = document.createTextNode("Frete grátis e encomenda sob medida")
+    setTimeout(function(){
+        p.appendChild(txt)
+        msg.appendChild(p)
+        fig.appendChild(msg)
+    }, 1000);
+
+   setTimeout(function(){
+    msg.style.opacity = 1
+   }, 2000);
+
+     setTimeout(function() {
+        msg.style.opacity = 0
+    }, 7000);
+
+    setTimeout(function() {
+        msg.remove()
+    }, 9000);
+    
+}
